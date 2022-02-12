05b4e79520e9e6dda377ae01847243e4ac934614
baraaAlosaily <seelbaraa@gmail.com>
2022-02-12 23:42:13 +0200

    complete list component with geolocation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import Header from "./components/Header/Header"
 import List from "./components/List/List";
 import Map from "./components/Map/Map";
-
 import { CssBaseline,Grid } from '@material-ui/core';
 
+import { getPlacesData } from './api';
+
+
 const App = () => {
+
+  const[places,setPlaces]=useState([]);
+  const[coordinates,setCoordinates]=useState({lan:0,lng:0});
+  const[bounds,setBounds]=useState(null);
+
+  // useEffect(()=>{
+  //   navigator.geolocation.getCurrentPosition(({coords:{latitude,longitude}})=>{
+  //      setCoordinates({lat:latitude,lng:longitude})
+  //   })
+  // },[])
+
+  useEffect(()=>{
+    console.log(bounds,coordinates);
+    getPlacesData()
+    .then((data)=>{
+      console.log(data);
+      setPlaces(data)
+    })
+  },[bounds,coordinates]);
+  
   return (
     <>
       <CssBaseline/>
@@ -15,7 +37,11 @@ const App = () => {
           <List/>
         </Grid>
         <Grid item xs={12} md={8} >
-          <Map/>
+          <Map
+          setCoordinates={setCoordinates}
+          setBounds={setBounds}
+          coordinates={coordinates}
+          />
         </Grid>
       </Grid>
     </>
